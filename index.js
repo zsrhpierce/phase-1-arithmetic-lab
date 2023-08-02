51a76d17f4fd7d29df621d324a2e57ef76c1e414
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-01 22:47:48 -0600

    math

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+console.log(random);
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+let max = Math.max(1,5,20);
